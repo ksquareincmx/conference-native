b9a3a6703897f01a3a4b418db7646e0ddf1de0ad
Abraham Escamilla Pinelo <aep0492@gmail.com>
2019-07-26 12:21:39 -0500

    created CRCellButton file

---
+++ src/components/common/CRCellButton.js
@@ -0,0 +1,22 @@
+"use-strict";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+class CRCellButton extends Component {
+  render() {
+    <View style={styles.mainContainer}>
+      <TouchableOpacity>
+          <Text>Accept</Text>
+      </TouchableOpacity>
+    </View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  }
+})
+
+
+export { CRCellButton };

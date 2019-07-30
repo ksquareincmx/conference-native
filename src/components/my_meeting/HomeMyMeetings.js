ef947eec54716bf7d9c562a12a84759d8132c47a
Abraham Escamilla Pinelo <aep0492@gmail.com>
2019-07-30 12:21:07 -0500

    HomeMyMeetings to functional component. renderItem to a Item component

--- src/components/my_meeting/HomeMyMeetings.js
+++ src/components/my_meeting/HomeMyMeetings.js
@@ -1,37 +1,36 @@
 "use-strict";
-import React, { Component } from "react";
+import React from "react";
 import { View, StyleSheet, FlatList, Dimensions } from "react-native";
-import { CRHeader } from "src/components/common"
-import MeetingCell from "src/components/my_meeting/MeetingCell"
+import { CRHeader } from "src/components/common";
+import MeetingCell from "src/components/my_meeting/MeetingCell";
 
 const cellWidth = Dimensions.get("window").width;
-const data = [{ id: "0", title: "hola" }, { id: "1", title: "Hola" }, { id: "2", title: "Hola" }, { id: "3", title: "Hola" }];
+const data = [
+  { id: "0", title: "hola" },
+  { id: "1", title: "Hola" },
+  { id: "2", title: "Hola" },
+  { id: "3", title: "Hola" }
+];
 
-class HomeMyMeetings extends Component {
-  render() {
-    return (
-      <View style={styles.main}>
-        <CRHeader leftText="Profile" />
-        <FlatList
-          data={data}
-          renderItem={this.renderItem}
-          keyExtractor={item => item.id}
-        />
-      </View>
-    );
-  }
-
-  renderItem = ({ item }) => (
-    <View style={{ width: cellWidth, height: cellWidth * 0.5, padding: 5 }}>
-      <MeetingCell onPress={this.onPressAppoint} text={item.title} />
+const HomeMyMeetings = () => {
+  return (
+    <View style={styles.main}>
+      <CRHeader leftText="Profile" />
+      <FlatList data={data} renderItem={Item} keyExtractor={item => item.id} />
     </View>
   );
-}
+};
+
+const Item = ({ item }) => (
+  <View style={{ width: cellWidth, height: cellWidth * 0.5, padding: 5 }}>
+    <MeetingCell text={item.title} />
+  </View>
+);
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: '#FFDADB'
+    backgroundColor: "#FFDADB"
   }
 });
 
